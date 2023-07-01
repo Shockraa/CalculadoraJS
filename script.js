6e49df1cce870ae0427e9844b782e2ae95d01c7e
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-01 18:53:03 -0300

    Criado repositório para calculadora em JS

---
+++ script.js
@@ -0,0 +1,62 @@
+function somar() {
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r = parseFloat(a) + parseFloat(b);
+    document.f1.c.value = r;
+
+}
+
+function subtrair() {
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r = parseFloat(a) - parseFloat(b);
+    document.f1.c.value = r;
+
+}
+function multiplicar() {
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r = parseFloat(a) * parseFloat(b);
+    document.f1.c.value = r;
+
+}
+function dividir() {
+
+    var a = document.f1.a.value;
+    var b = document.f1.b.value;
+    var r = document.f1.c.value;
+    r = parseFloat(a) / parseFloat(b);
+    document.f1.c.value = r;
+
+}
+function calcular() {
+
+    var operacao = document.f1.operacao.value;
+
+    switch (operacao) {
+        case "somar":
+            somar();
+            break;
+
+        case "subtrair":
+            subtrair();
+            break;
+
+        case "multiplicar":
+            multiplicar();
+            break;
+
+        case "dividir":
+            dividir();
+            break;
+
+        default:
+            break;
+    }
+}
